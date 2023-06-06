1f4bcd154a627cd738221ae5600c4554fec4ab3e
Niklas Gund <kevinfarber58@gmail.com>
2023-06-06 23:16:57 +0200

    fix: blank page on app startup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import NavBar from './components/NavBar.vue';
 import ThreeLists from "./components/ThreeLists.vue"
+import { onMounted } from 'vue';
+
+const router = useRouter();
+
+onMounted(() => router.push("/"))
 
 </script>
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="bg-gray-100 py-4 rounded-b-lg px-4 flex">
     <div class="my-auto font-bold text-4xl">DO YOUR 💩</div>
-    <!--! add real statistics  -->
-    <div class="my-auto ml-auto">Backlog: 20 Doing: 10 Done: 5</div>
+    <div class="my-auto ml-auto">Backlog: {{ DYSStore.backlog.value.length }} Doing: {{ DYSStore.backlog.value.length }} Done: {{ DYSStore.backlog.value.length }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import DYSStore from '../store/localStoragePersistor';
+
 
 </script>
